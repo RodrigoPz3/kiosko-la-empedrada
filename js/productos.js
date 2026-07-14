5065ba4583cfbdd4236940bc8777f81249750845
Rodrigopz3 <ropney11@icloud.com>
2026-07-14 21:41:53 +0000

    14 jul 2026, 3:41 p.m.

--- js/productos.js
+++ js/productos.js
@@ -3,252 +3,252 @@ const productos = [
         nombre: "Buldak Ramen Carbonara 130gr",
         precio: 42,
         categoria: "Sopas",
-        imagen: "imagenes/productos/buldak-ramen-carbonara-130gr.png",
+        imagen: "../imagenes/productos/buldak-ramen-carbonara-130gr.webp",
         disponible: true
     },
     {
         nombre: "Buldak Ramen Cream Carbonara 140gr",
         precio: 42,
         categoria: "Sopas",
-        imagen: "imagenes/productos/buldak-ramen-cream-carbonara-140gr.png",
+        imagen: "../imagenes/productos/buldak-ramen-cream-carbonara-140gr.webp",
         disponible: true
     },
     {
         nombre: "Buldak Ramen 2x 140gr",
         precio: 42,
         categoria: "Sopas",
-        imagen: "imagenes/productos/buldak-ramen-2x-140gr.png",
+        imagen: "../imagenes/productos/buldak-ramen-2x-140gr.webp",
         disponible: true
     },
     {
         nombre: "Buldak Ramen original 140gr",
         precio: 42,
         categoria: "Sopas",
-        imagen: "imagenes/productos/buldak-ramen-original-140gr.png",
+        imagen: "../imagenes/productos/buldak-ramen-original-140gr.webp",
         disponible: true
     },
     {
         nombre: "Buldak Ramen Carbonara cup 80gr",
         precio: 44,
         categoria: "Sopas",
-        imagen: "imagenes/productos/buldak-ramen-carbonara-cup-80gr.png",
+        imagen: "../imagenes/productos/buldak-ramen-carbonara-cup-80gr.webp",
         disponible: true
     },
     {
         nombre: "Buldak Ramen Habanero Lime Big Bowl 110gr",
         precio: 52,
         categoria: "Sopas",
-        imagen: "imagenes/productos/buldak-ramen-habanero-lime-big-bowl-110gr.png",
+        imagen: "../imagenes/productos/buldak-ramen-habanero-lime-big-bowl-110gr.webp",
         disponible: true
     },
     {
         nombre: "Pocky Chocolate 45gr",
         precio: 46,
         categoria: "Galletas",
-        imagen: "imagenes/productos/pocky-chocolate-40gr.png",
+        imagen: "../imagenes/productos/pocky-chocolate-40gr.webp",
         disponible: true
     },
     {
         nombre: "Pocky Fresa 45gr",
         precio: 42,
         categoria: "Galletas",
-        imagen: "imagenes/productos/pocky-fresa-40gr.webp",
+        imagen: "../imagenes/productos/pocky-fresa-40gr.webp",
         disponible: true
     },
     {
         nombre: "Peelerz Uva 65gr",
         precio: 44,
         categoria: "Dulces",
-        imagen: "imagenes/productos/peelerz-uva-65gr.webp",
+        imagen: "../imagenes/productos/peelerz-uva-65gr.webp",
         disponible: true
     },
     {
         nombre: "Peelerz Fresa 65gr",
         precio: 44,
         categoria: "Dulces",
-        imagen: "imagenes/productos/peelerz-fresa-65gr.webp",
+        imagen: "../imagenes/productos/peelerz-fresa-65gr.webp",
         disponible: true
     },
     {
         nombre: "Peelerz Mango 65gr",
         precio: 44,
         categoria: "Dulces",
-        imagen: "imagenes/productos/peelerz-mango-65gr.webp",
+        imagen: "../imagenes/productos/peelerz-mango-65gr.webp",
         disponible: true
     },
     {
         nombre: "Peelerz Piña 65gr",
         precio: 44,
         categoria: "Dulces",
-        imagen: "imagenes/productos/peelerz-piña-65gr.webp",
+        imagen: "../imagenes/productos/peelerz-piña-65gr.webp",
         disponible: true
     },
     {
         nombre: "Peelerz Platano 65gr",
         precio: 44,
         categoria: "Dulces",
-        imagen: "imagenes/productos/peelerz-platano-65gr.webp",
+        imagen: "../imagenes/productos/peelerz-platano-65gr.webp",
         disponible: true
     },
     {
         nombre: "Peelerz Durazno 65gr",
         precio: 44,
         categoria: "Dulces",
-        imagen: "imagenes/productos/peelerz-durazno-65gr.webp",
+        imagen: "../imagenes/productos/peelerz-durazno-65gr.webp",
         disponible: true
     },
     {
         nombre: "Meiji Yan Yan Chocolate 57gr",
         precio: 54,
         categoria: "Galletas",
-        imagen: "imagenes/productos/meiji-yan-yan-chocolate-57gr.webp",
+        imagen: "../imagenes/productos/meiji-yan-yan-chocolate-57gr.webp",
         disponible: true
     },
     {
         nombre: "Meiji Yan Yan Fresa 57gr",
         precio: 54,
         categoria: "Galletas",
-        imagen: "imagenes/productos/meiji-yan-yan-fresa-57gr.webp",
+        imagen: "../imagenes/productos/meiji-yan-yan-fresa-57gr.webp",
         disponible: true
     },
     {
         nombre: "Royal Mochi Coco 120gr",
         precio: 54,
         categoria: "Dulces",
-        imagen: "imagenes/productos/royal-mochi-coco-120gr.webp",
+        imagen: "../imagenes/productos/royal-mochi-coco-120gr.webp",
         disponible: true
     },
     {
         nombre: "Royal Mochi Cheesecake Fresa 120gr",
         precio: 54,
         categoria: "Dulces",
-        imagen: "imagenes/productos/royal-mochi-cheesecake-fresa-120gr.webp",
+        imagen: "../imagenes/productos/royal-mochi-cheesecake-fresa-120gr.webp",
         disponible: true
     },
     {
         nombre: "Royal Mochi Caramelo 120gr",
         precio: 54,
         categoria: "Dulces",
-        imagen: "imagenes/productos/royal-mochi-caramelo-120gr.webp",
+        imagen: "../imagenes/productos/royal-mochi-caramelo-120gr.webp",
         disponible: true
     },
     {
         nombre: "Royal Mochi Chocolate 120gr",
         precio: 54,
         categoria: "Dulces",
-        imagen: "imagenes/productos/royal-mochi-chocolate-120gr.webp",
+        imagen: "../imagenes/productos/royal-mochi-chocolate-120gr.webp",
         disponible: true
     },
     {
         nombre: "Royal Mochi Cacahuate 120gr",
         precio: 54,
         categoria: "Dulces",
-        imagen: "imagenes/productos/royal-mochi-cacahuate-120gr.webp",
+        imagen: "../imagenes/productos/royal-mochi-cacahuate-120gr.webp",
         disponible: true
     },
     {
         nombre: "Royal Mochi Chocomenta 120gr",
         precio: 54,
         categoria: "Dulces",
-        imagen: "imagenes/productos/royal-mochi-chocomenta-120gr.webp",
+        imagen: "../imagenes/productos/royal-mochi-chocomenta-120gr.webp",
         disponible: true
     },
     {
         nombre: "Royal Mochi Tiramisu 120gr",
         precio: 54,
         categoria: "Dulces",
-        imagen: "imagenes/productos/royal-mochi-tiramisu-120gr.webp",
+        imagen: "../imagenes/productos/royal-mochi-tiramisu-120gr.webp",
         disponible: true
     },
     {
         nombre: "Hata Ramune Mora Azul 200ml",
         precio: 48,
         categoria: "Bebidas",
-        imagen: "imagenes/productos/hata-ramune-mora-azul-200ml.webp",
+        imagen: "../imagenes/productos/hata-ramune-mora-azul-200ml.webp",
         disponible: true
     },
     {
         nombre: "Hata Ramune Yuzu 200ml",
         precio: 48,
         categoria: "Bebidas",
-        imagen: "imagenes/productos/hata-ramune-yuzu-200ml.webp",
+        imagen: "../imagenes/productos/hata-ramune-yuzu-200ml.webp",
         disponible: true
     },
     {
         nombre: "Binggrae Fresa 200ml",
         precio: 38,
         categoria: "Bebidas",
-        imagen: "imagenes/productos/binggrae-fresa-200ml.webp",
+        imagen: "../imagenes/productos/binggrae-fresa-200ml.webp",
         disponible: false
     },
     {
         nombre: "Binggrae Cafe 200ml",
         precio: 38,
         categoria: "Bebidas",
-        imagen: "imagenes/productos/binggrae-cafe-200ml.webp",
+        imagen: "../imagenes/productos/binggrae-cafe-200ml.webp",
         disponible: true
     },
     {
         nombre: "Binggrae Taro 200ml",
         precio: 38,
         categoria: "Bebidas",
-        imagen: "imagenes/productos/binggrae-taro-200ml.webp",
+        imagen: "../imagenes/productos/binggrae-taro-200ml.webp",
         disponible: true
     },
     {
         nombre: "Binggrae Banana 200ml",
         precio: 38,
         categoria: "Bebidas",
-        imagen: "imagenes/productos/binggrae-banana-200ml.webp",
+        imagen: "../imagenes/productos/binggrae-banana-200ml.webp",
         disponible: true
     },
     {
         nombre: "Yopokki Botana Picante 50gr",
         precio: 44,
         categoria: "Botanas",
-        imagen: "imagenes/productos/yopokki-botana-picante-50gr.webp",
+        imagen: "../imagenes/productos/yopokki-botana-picante-50gr.webp",
         disponible: true
     },
     {
         nombre: "Yopokki Botana Pizza 50gr",
         precio: 44,
         categoria: "Botanas",
-        imagen: "imagenes/productos/yopokki-botana-pizza-50gr.webp",
+        imagen: "../imagenes/productos/yopokki-botana-pizza-50gr.webp",
         disponible: false
     },
     {
         nombre: "Okf Refresco Limonada azul 350ml",
         precio: 32,
         categoria: "Bebidas",
-        imagen: "imagenes/productos/okf-refresco-limonada-azul-350ml.webp",
+        imagen: "../imagenes/productos/okf-refresco-limonada-azul-350ml.webp",
         disponible: true
     },
     {
         nombre: "Bourbon Petit Galletas Matcha 40gr",
         precio: 25,
         categoria: "Galletas",
-        imagen: "imagenes/productos/bourbon-petit-galletas-matcha-40gr.webp",
+        imagen: "../imagenes/productos/bourbon-petit-galletas-matcha-40gr.webp",
         disponible: true
     },
     {
         nombre: "Bourbon Petit Galletas Mantequilla 42gr",
         precio: 25,
         categoria: "Galletas",
-        imagen: "imagenes/productos/bourbon-petit-galletas-mantequilla-42gr.webp",
+        imagen: "../imagenes/productos/bourbon-petit-galletas-mantequilla-42gr.webp",
         disponible: true
     },
     {
         nombre: "Cantabile Sandia 230ml",
         precio: 30,
         categoria: "Bebidas",
-        imagen: "imagenes/productos/cantabile-sandia-230ml.webp",
+        imagen: "../imagenes/productos/cantabile-sandia-230ml.webp",
         disponible: true
     },
     {
         nombre: "Cantabile Cafe Vainilla 230ml",
         precio: 30,
         categoria: "Bebidas",
-        imagen: "imagenes/productos/cantabile-cafe-vainilla-230ml.webp",
+        imagen: "../imagenes/productos/cantabile-cafe-vainilla-230ml.webp",
         disponible: true
     },
 ];
