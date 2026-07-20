ac55fd5744bf23cdc2c0eb01bd1f0ebe2ce1f5d8
Rodrigopz3 <ropney11@icloud.com>
2026-07-20 21:31:43 +0000

    20 jul 2026, 3:31 p.m.

--- js/productos.js
+++ js/productos.js
@@ -1,10 +1,8 @@
 const productos = [
-    // BULDAK CARBONARA
     {
         nombre: "Buldak Carbonara",
-        imagen: "imagenes/productos/buldak-ramen-carbonara-130gr-10pz.webp",
+        imagen: "imagenes/productos/buldak-ramen-carbonara.webp",
         disponible: true,
-
         precios: [
             {
                 titulo: "10 Unidades",
@@ -19,5 +17,24 @@ const productos = [
                 precio: 32.90
             }
         ]
-    }
+    },
+    {
+        nombre: "Buldak Carbonara",
+        imagen: "imagenes/productos/buldak-ramen-carbonara.webp",
+        disponible: true,
+        precios: [
+            {
+                titulo: "10 Unidades",
+                precio: 34.80
+            },
+            {
+                titulo: "Caja (20 Unidades)",
+                precio: 33.40
+            },
+            {
+                titulo: "Caja (40 Unidades)",
+                precio: 32.90
+            }
+        ]
+    },
 ];
