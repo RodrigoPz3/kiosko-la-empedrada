d1fbd1925c0d185a542c1da28a03e43fabc15ae3
Rodrigopz3 <ropney11@icloud.com>
2026-07-20 21:40:43 +0000

    20 jul 2026, 3:40 p.m.

--- js/productos.js
+++ js/productos.js
@@ -37,4 +37,80 @@ const productos = [
             }
         ]
     },
+    {
+        nombre: "Buldak Cream Carbonara",
+        imagen: "imagenes/productos/buldak-ramen-cream-carbonara.webp",
+        disponible: true,
+        precios: [
+            {
+                titulo: "10 Unidades",
+                precio: 34.80
+            },
+            {
+                titulo: "Caja (20 Unidades)",
+                precio: 33.40
+            },
+            {
+                titulo: "Caja (40 Unidades)",
+                precio: 32.90
+            }
+        ]
+    },
+    {
+        nombre: "Buldak Cream Carbonara",
+        imagen: "imagenes/productos/buldak-ramen-cream-carbonara.webp",
+        disponible: true,
+        precios: [
+            {
+                titulo: "10 Unidades",
+                precio: 34.80
+            },
+            {
+                titulo: "Caja (20 Unidades)",
+                precio: 33.40
+            },
+            {
+                titulo: "Caja (40 Unidades)",
+                precio: 32.90
+            }
+        ]
+    },
+    {
+        nombre: "Buldak Cream Carbonara",
+        imagen: "imagenes/productos/buldak-ramen-cream-carbonara.webp",
+        disponible: true,
+        precios: [
+            {
+                titulo: "10 Unidades",
+                precio: 34.80
+            },
+            {
+                titulo: "Caja (20 Unidades)",
+                precio: 33.40
+            },
+            {
+                titulo: "Caja (40 Unidades)",
+                precio: 32.90
+            }
+        ]
+    },
+    {
+        nombre: "Buldak Cream Carbonara",
+        imagen: "imagenes/productos/buldak-ramen-cream-carbonara.webp",
+        disponible: true,
+        precios: [
+            {
+                titulo: "10 Unidades",
+                precio: 34.80
+            },
+            {
+                titulo: "Caja (20 Unidades)",
+                precio: 33.40
+            },
+            {
+                titulo: "Caja (40 Unidades)",
+                precio: 32.90
+            }
+        ]
+    },
 ];
