eb831b1dfbc8310f815f5c22966b3331b3f16004
Rodrigopz3 <ropney11@icloud.com>
2026-07-20 19:24:23 +0000

    20 jul 2026, 1:24 p.m.

--- js/productos.js
+++ js/productos.js
@@ -7,16 +7,16 @@ const productos = [
 
         precios: [
             {
-                titulo: "Caja (40 pz)",
-                precio: 33.00
+                titulo: "10 Unidades",
+                precio: 34.80
             },
             {
-                titulo: "2 cajas",
-                precio: 32.50
+                titulo: "Caja (20 Unidades)",
+                precio: 33.40
             },
             {
-                titulo: "100 pz+",
-            precio: 31.80
+                titulo: "Caja (40 Unidades)",
+                precio: 32.90
             }
         ]
     }
