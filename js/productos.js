ce7358c0f088bc57a3c60422e414ee158b99bd99
Rodrigopz3 <ropney11@icloud.com>
2026-07-20 07:00:08 +0000

    20 jul 2026, 1:00 a.m.

--- js/productos.js
+++ js/productos.js
@@ -1,4 +1,5 @@
 const productos = [
+    // BULDAK CARBONARA
     {
         nombre: "Buldak Ramen Carbonara 130gr 10pz",
         precio: 348,
@@ -13,6 +14,7 @@ const productos = [
         imagen: "/imagenes/productos/buldak-ramen-carbonara-130gr-40pz.webp",
         disponible: false
     },
+    // BULDAK CREAM CARBONARA
     {
         nombre: "Buldak Ramen Cream Carbonara 140gr 10pz",
         precio: 348,
@@ -27,6 +29,7 @@ const productos = [
         imagen: "/imagenes/productos/buldak-ramen-cream-carbonara-140gr-40pz.webp",
         disponible: false
     },
+    // BULDAK ORIGINAL
     {
         nombre: "Buldak Ramen Original 140gr 10pz",
         precio: 348,
@@ -41,12 +44,13 @@ const productos = [
         imagen: "/imagenes/productos/buldak-ramen-original-140gr-40pz.webp",
         disponible: false
     },
+    // BULDAK 2X
     {
         nombre: "Buldak Ramen 2x 140gr 10pz",
         precio: 348,
         categoria: "Sopas",
         imagen: "/imagenes/productos/buldak-ramen-2x-140gr-10pz.webp",
-        disponible: false
+        disponible: true
     },
     {
         nombre: "Buldak Ramen 2x 140gr Caja Con 40pz",
@@ -55,32 +59,49 @@ const productos = [
         imagen: "/imagenes/productos/buldak-ramen-2x-140gr-40pz.webp",
         disponible: false
     },
+    // CANTABILE CAFE VAINILLA
     {
         nombre: "Cantabile Cafe Vainilla 230ml 10pz",
-        precio: 249,
+        precio: 248,
         categoria: "Bebidas",
         imagen: "/imagenes/productos/cantabile-cafe-230ml-10pz.webp",
         disponible: true
     },
     {
         nombre: "Cantabile Cafe Vainilla 230ml 50pz",
-        precio: 249,
+        precio: 1178,
         categoria: "Bebidas",
         imagen: "/imagenes/productos/cantabile-cafe-230ml-50pz.webp",
-        disponible: true
+        disponible: false
     },
+    // CANTABILE SANDIA
     {
         nombre: "Cantabile Sandia 230ml 10pz",
-        precio: 249,
+        precio: 248,
         categoria: "Bebidas",
         imagen: "/imagenes/productos/cantabile-sandia-230ml-10pz.webp",
         disponible: true
     },
     {
         nombre: "Cantabile Sandia 230ml 50pz",
-        precio: 249,
+        precio: 1178,
         categoria: "Bebidas",
         imagen: "/imagenes/productos/cantabile-sandia-230ml-50pz.webp",
+        disponible: false
+    },
+    // MOCHIS CACAHUATE
+    {
+        nombre: "Mochis Cacahuate 120gr 5pz",
+        precio: 248,
+        categoria: "Bebidas",
+        imagen: "/imagenes/productos/mochis-cacahuate-120gr-5pz.webp",
         disponible: true
+    },
+    {
+        nombre: "Mochis Cacahuate 120gr 12pz",
+        precio: 1178,
+        categoria: "Bebidas",
+        imagen: "/imagenes/productos/mochis-cacahuate-120gr-12pz.webp",
+        disponible: false
     },
 ];
