869505e0d73bba09c6ccb3912a54a206149a68d1
Rodrigopz3 <ropney11@icloud.com>
2026-07-20 19:17:34 +0000

    20 jul 2026, 1:17 p.m.

--- js/productos.js
+++ js/productos.js
@@ -1,13 +1,23 @@
 const productos = [
     // BULDAK CARBONARA
     {
-        nombre: "Buldak Carbonara 140g",
+        nombre: "Buldak Carbonara",
         imagen: "imagenes/buldak-carbonara.webp",
         disponible: true,
+
         precios: [
-            { cantidad: "1 caja", precio: 33.00 },
-            { cantidad: "2 cajas", precio: 32.50 },
-            { cantidad: "100 pz+", precio: 31.80 }
+            {
+                titulo: "Caja (40 pz)",
+                precio: 33.00
+            },
+            {
+                titulo: "2 cajas",
+                precio: 32.50
+            },
+            {
+                titulo: "100 pz+",
+            precio: 31.80
+            }
         ]
     }
 ];
